bfeef08f83ca1959c53257b6fb012e4e86c2760d
TessyJames28 <tessyjames28@gmail.com>
2023-08-01 20:23:46 +0100

    simple fask setup

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,11 +6,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route(/)
+@app.route('/', strict_slashes=False)
 def index():
     """index function"""
     return render_template('0-index.html')
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
