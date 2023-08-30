722aac7730a8a3357c55745e004167dda523287b
TessyJames28 <tessyjames28@gmail.com>
2023-08-30 20:22:44 +0100

    Create the Job processor

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -2,8 +2,8 @@ import kue from 'kue';
 const queue = kue.createQueue()
 
 const jobData = {
-	phoneNumber: 'string',
-  message: 'string',
+  phoneNumber: '4153518780',
+  message: 'This is the code to verify your account',
 }
 
 const job = queue.create('push_notification_code', jobData).save(err => {
